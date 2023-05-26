d24e43cf5db07fe3aaa1d9fbb2a84fa6257071a5
Maksim <bogdanenko1996@gmail.com>
2023-05-26 17:12:03 +0300

    User Service logic/ Add validation for pass and roles

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -0,0 +1,18 @@
+package ru.kata.spring.boot_security.demo.services;
+
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.validation.BindingResult;
+import ru.kata.spring.boot_security.demo.models.User;
+
+public interface UserService extends UserDetailsService {
+    Iterable<User> getAllUsers();
+    void removeUserById(Long id);
+    void saveNewUserProfile(User user);
+    void checkUserProfile(User user, BindingResult bindingResult);
+    User getById(Long id);
+
+    void updateUserProfile(User user);
+
+   Boolean isUserExistsWithEmail(String email);
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -0,0 +1,85 @@
+package ru.kata.spring.boot_security.demo.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import ru.kata.spring.boot_security.demo.DAO.UserRepository;
+import ru.kata.spring.boot_security.demo.models.User;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public Iterable<User> getAllUsers() {
+       return userRepository.findAll();
+    }
+    @Transactional
+    public void removeUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+
+    @Transactional
+    public void saveNewUserProfile(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void checkUserProfile(User user, BindingResult bindingResult) {
+        if(user.getPassword().length()==0){
+            bindingResult.rejectValue("password", "error.password", "Password shouldn`t be Empty");
+        }
+        if(user.getUserRoles().size()==0){
+            bindingResult.rejectValue("userRoles", "error.userRoles", "User must have at least 1 role");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public User getById(Long id) {
+        return userRepository.findById(id).orElse(new User());
+    }
+
+    @Transactional
+    public void updateUserProfile(User user) {
+        Optional<User> curUser = userRepository.findById(user.getId());
+        if(curUser.isPresent()){
+            User updatedUser = curUser.get();
+            updatedUser.setAge(user.getAge());
+            updatedUser.setAvatar(user.getAvatar());
+            updatedUser.setCity(user.getCity());
+            updatedUser.setEmail(user.getEmail());
+            updatedUser.setLastName(user.getLastName());
+            updatedUser.setFirstName(user.getFirstName());
+            userRepository.save(updatedUser);
+        }
+
+
+    }
+
+
+    @Transactional(readOnly = true)
+    public Boolean isUserExistsWithEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    @Transactional(readOnly = true)
+    public User loadUserByUsername(String email) {
+        User user  = userRepository.findByEmail(email);
+        if(user == null) {
+            throw new UsernameNotFoundException(email);
+        }
+        return user;
+    }
+}
