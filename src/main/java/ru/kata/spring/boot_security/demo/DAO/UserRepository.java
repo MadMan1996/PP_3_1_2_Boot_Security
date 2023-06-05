0812b8e8b89bc93ac193412e0ac08cd4289cfaf7
Maksim <bogdanenko1996@gmail.com>
2023-06-05 12:10:20 +0300

    Add Entity Graph for userRoles to avoid LaziInitializationException

--- src/main/java/ru/kata/spring/boot_security/demo/DAO/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/DAO/UserRepository.java
@@ -1,10 +1,13 @@
 package ru.kata.spring.boot_security.demo.DAO;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import ru.kata.spring.boot_security.demo.models.User;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface UserRepository extends CrudRepository<User, Long> {
     boolean existsByEmail(String email);
+    @EntityGraph(attributePaths = {"userRoles"})
     User findByEmail(String email);
 
 }
