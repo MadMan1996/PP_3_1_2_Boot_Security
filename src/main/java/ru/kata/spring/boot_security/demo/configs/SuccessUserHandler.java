dc7605126285a464b03e3394def4dc8230798cc6
Maksim <bogdanenko1996@gmail.com>
2023-06-21 13:31:00 +0300

    Change reditrection to default page after success login

--- src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
@@ -15,11 +15,6 @@ public class SuccessUserHandler implements AuthenticationSuccessHandler {
     // Spring Security использует объект Authentication, пользователя авторизованной сессии.
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
-        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        if(roles.contains("ROLE_ADMIN")){
-            httpServletResponse.sendRedirect("/admin/users");
-        } else if (roles.contains("ROLE_USER")) {
-            httpServletResponse.sendRedirect("/user");
-        }
+        httpServletResponse.sendRedirect("/main");
     }
 }
